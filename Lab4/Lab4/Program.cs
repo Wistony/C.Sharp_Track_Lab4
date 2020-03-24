d942cf2029b1f370fedb6dfdb15b247dbcfdd660
Max.Androshchuk <mmaksimm921@gmail.com>
2020-03-24 22:30:44 +0200

    simple sample added

--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -6,7 +6,50 @@ namespace Lab4
     {
         private static void Main(string[] args)
         {
-            
+            var ukraineLibrary = new Library();
+            ukraineLibrary.AddBook(new Book
+                {
+                    Title = "Ukraina v ogni",  
+                    Author = new Author
+                    {
+                        Fullname = "Oleksandr Dovzhenko",
+                        YearOfBirth = 1894,
+                        YearOfDeath = 1956
+                    },
+                    PublishingHouse = "Folio",
+                    YearOfPublishing = 2010,
+                    Value = 150
+                },
+                new Book
+                {
+                    Title = "Zacharovana Desna",  
+                    Author = new Author
+                    {
+                        Fullname = "Oleksandr Dovzhenko",
+                        YearOfBirth = 1894,
+                        YearOfDeath = 1956
+                    },
+                    PublishingHouse = "Folio",
+                    YearOfPublishing = 2011,
+                    Value = 150
+                },
+                new Book
+                {
+                    Title = "Chorna Rada",  
+                    Author = new Author
+                    {
+                        Fullname = "Panteleimon Kulish",
+                        YearOfBirth = 1819,
+                        YearOfDeath = 1897
+                    },
+                    PublishingHouse = "Kristal Book",
+                    YearOfPublishing = 2015,
+                    Value = 200
+                },
+                
+                
+                
+                );
         }
     }
 }
