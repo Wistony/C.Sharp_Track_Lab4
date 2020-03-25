580bbf983e3c4ae8c7897e2b00ab6334223dcae6
Max.Androshchuk <mmaksimm921@gmail.com>
2020-03-25 17:40:47 +0200

    some fix

--- Lab4/Lab4/Book.cs
+++ Lab4/Lab4/Book.cs
@@ -6,7 +6,7 @@ namespace Lab4
     public class Book
     {
         public string Title { get; set; }
-        public Author Author { get; set; }
+        public string Author { get; set; }
         public string PublishingHouse { get; set; }
         public int YearOfPublishing { get; set; }
         public int Value { get; set; }
@@ -15,7 +15,7 @@ namespace Lab4
         {
             
         }
-        public Book(string title, Author author, string publishingHouse, int yearOfPublishing, int value )
+        public Book(string title, string author, string publishingHouse, int yearOfPublishing, int value )
         {
             Title = title;
             Author = author;
@@ -26,34 +26,9 @@ namespace Lab4
 
         public override string ToString()
         {
-            return "(title=\"" + this.Title + "\"; author={" + Author.ToString() + "};" +
+            return "(title=\"" + this.Title + "\"; author=\"" + this.Author + "\";" +
                    " publishingHouse=\"" + this.PublishingHouse + "\"; yearOfPublishing=\"" +
                    +this.YearOfPublishing + "\"; value=\"" + this.Value + "\";";
-        }
-    }
-
-    public class Author
-    {
-        public string Fullname { get; set; }
-        public int YearOfBirth { get; set; }
-        public int? YearOfDeath { get; set; }
-
-        public Author()
-        {
-            
-        }
-
-        public Author(string fullname, int yearOfBirth, int? yearOfDeath)
-        {
-            Fullname = fullname;
-            YearOfBirth = yearOfBirth;
-            YearOfDeath = yearOfDeath;
-        }
-
-        public override string ToString()
-        {
-            return "fullname=\"" + this.Fullname + "\"; yearOfBirth=\"" +
-                   +this.YearOfBirth + "\"; yearOfDeath=\"" + this.YearOfDeath + "\";";
         }
     }
 }
--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -4,23 +4,16 @@ using System.Linq;
 
 namespace Lab4
 {
-    
-
     public static class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
             var ukraineLibrary = new List<Book>
             {
                 new Book
                 {
                     Title = "Ukraina v ogni",
-                    Author = new Author
-                    {
-                        Fullname = "Oleksandr Dovzhenko",
-                        YearOfBirth = 1894,
-                        YearOfDeath = 1956
-                    },
+                    Author = "Oleksandr Dovzhenko",
                     PublishingHouse = "Folio",
                     YearOfPublishing = 2010,
                     Value = 150
@@ -28,39 +21,88 @@ namespace Lab4
                 new Book
                 {
                     Title = "Zacharovana Desna",
-                    Author = new Author
-                    {
-                        Fullname = "Oleksandr Dovzhenko",
-                        YearOfBirth = 1894,
-                        YearOfDeath = 1956
-                    },
+                    Author = "Oleksandr Dovzhenko",
                     PublishingHouse = "Folio",
                     YearOfPublishing = 2011,
                     Value = 150
                 },
                 new Book
                 {
-                    Title = "Chorna Rada",
-                    Author = new Author
-                    {
-                        Fullname = "Panteleimon Kulish",
-                        YearOfBirth = 1819,
-                        YearOfDeath = 1897
-                    },
-                    PublishingHouse = "Kristal Book",
-                    YearOfPublishing = 2015,
+                    Title = "Kaidasheva Simya",
+                    Author = "Ivan Nechyi-Levytskiy",
+                    PublishingHouse = "Ranok",
+                    YearOfPublishing = 2008,
                     Value = 200
                 }
+                
             };
 
+            var delimiter = new string('=', 180);
+ 
+//------------------------------------------------------------------------------------------------------------------            
+          
+            Console.WriteLine("1. Sorted list of all books\n");
+            var query1 =
+                from x in ukraineLibrary
+                orderby x.Title
+                select x.Title;
 
-            var query =
-                ukraineLibrary.Select(n => n.GetHashCode());
+            foreach (var x in query1)
+                Console.WriteLine($"\"{x}\"");
+            Console.WriteLine(delimiter); 
 
-            foreach (var i in query)
+//------------------------------------------------------------------------------------------------------------------            
+
+            Console.WriteLine("2. Books that to be published later than 2010 \n");
+            var query2 = ukraineLibrary
+                .Where(n => n.YearOfPublishing > 2010)
+                .OrderBy(n => n.YearOfPublishing)
+                .Select(n => (n.Title, n.YearOfPublishing));
+            
+            foreach (var x in query2)
+                Console.WriteLine($"{x}");
+            Console.WriteLine(delimiter);
+            
+//------------------------------------------------------------------------------------------------------------------            
+
+            Console.WriteLine("3. Books are grouped by authors \n");
+            var query3 =
+                from x in ukraineLibrary
+                group x.Title by x.Author
+                into g
+                select new
+                {
+                    Author = g.Key,
+                    Count = g.Count(),
+                    Books = from b in g
+                            select b
+                };
+
+            foreach (var x in query3)
             {
-                Console.WriteLine(i);
+                Console.WriteLine($"{x.Author} - {x.Count}");
+                foreach (var b in x.Books)
+                {
+                    Console.WriteLine($"\t\"{b}\"");
+                }
             }
+            Console.WriteLine(delimiter);
+
+//------------------------------------------------------------------------------------------------------------------            
+
+            Console.WriteLine("3. The cheapest book \n");
+            var query4 = ukraineLibrary
+                .OrderBy(n => n.Value)
+                .ThenBy(n => n.Title)
+                .Take(1)
+                .Select(n => (n.Author, n.Title, n.Value));
+
+            foreach (var x in query4)
+                Console.WriteLine($"{x}");
+                
+            
+            Console.WriteLine(delimiter);
+
         }
     }
 }
